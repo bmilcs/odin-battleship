7bf64c7708fe750211f0f88717afd6e18069ea18
Bryan Miller <bmilcs@yahoo.com>
2022-11-26 10:43:30 -0500

    Add describe() to ship & gameboard tests

--- src/modules/gameboard.test.js
+++ src/modules/gameboard.test.js
@@ -1,27 +1,31 @@
 import Gameboard from "./gameboard";
 
-test("create a new gameboard array, default size: 10", () => {
-  const board = Gameboard();
-  expect(board.getArray()).toStrictEqual([["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""],["","","","","","","","","",""]]);
-})
+describe("gameboard factory & method tests", () => {
 
-test("create a new gameboard array, non default size: 3", () => {
-  const board = Gameboard(3);
-  expect(board.getArray()).toEqual([["","",""],["","",""],["","",""]]);
-})
+  test("create a new gameboard array, default size: 10", () => {
+    const board = Gameboard();
+    expect(board.getArray()).toStrictEqual([["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", ""]]);
+  })
 
-test("get all positions between 2 positions", () => {
-  const board = Gameboard(3);
-  expect(board.getAllPositionsBetween([0, 0], [0, 2])).toEqual([[0, 0], [0, 1], [0, 2]]);
-})
+  test("create a new gameboard array, non default size: 3", () => {
+    const board = Gameboard(3);
+    expect(board.getArray()).toEqual([["", "", ""], ["", "", ""], ["", "", ""]]);
+  })
 
-test("get all positions between 2 positions", () => {
-  const board = Gameboard(6);
-  expect(board.getAllPositionsBetween([0, 0], [4, 0])).toEqual([[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]);
-})
+  test("get all positions between 2 positions", () => {
+    const board = Gameboard(3);
+    expect(board.getAllPositionsBetween([0, 0], [0, 2])).toEqual([[0, 0], [0, 1], [0, 2]]);
+  })
 
-test("placeShip changes gameboard array", () => {
-  const board = Gameboard(3);
-  board.placeShip([0, 0], [0, 2]);
-  expect(board.getArray()).not.toEqual([["","",""],["","",""],["","",""]]);
-})
+  test("get all positions between 2 positions", () => {
+    const board = Gameboard(6);
+    expect(board.getAllPositionsBetween([0, 0], [4, 0])).toEqual([[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]);
+  })
+
+  test("placeShip changes gameboard array", () => {
+    const board = Gameboard(3);
+    board.placeShip([0, 0], [0, 2]);
+    expect(board.getArray()).not.toEqual([["", "", ""], ["", "", ""], ["", "", ""]]);
+  })
+
+});
--- src/modules/ship.test.js
+++ src/modules/ship.test.js
@@ -1,18 +1,20 @@
 import Ship from "./ship";
 
-test("isSunk() on new Ship object = false", () => {
-  const shipFour = Ship(4);
-  expect(shipFour.isSunk()).toBe(false);
-});
+describe("Ship factory & methods tests", () => {
+  test("isSunk() on new Ship object = false", () => {
+    const shipFour = Ship(4);
+    expect(shipFour.isSunk()).toBe(false);
+  });
 
-test("isSunk() on ship(1) after 1 hit = true", () => {
-  const shipOne = Ship(1);
-  shipOne.hit();
-  expect(shipOne.isSunk()).toBe(true);
-});
+  test("isSunk() on ship(1) after 1 hit = true", () => {
+    const shipOne = Ship(1);
+    shipOne.hit();
+    expect(shipOne.isSunk()).toBe(true);
+  });
 
-test("isSunk() on ship(2) after 1 hit = false", () => {
-  const shipTwo = Ship(2);
-  shipTwo.hit();
-  expect(shipTwo.isSunk()).toBe(false);
+  test("isSunk() on ship(2) after 1 hit = false", () => {
+    const shipTwo = Ship(2);
+    shipTwo.hit();
+    expect(shipTwo.isSunk()).toBe(false);
+  });
 });
