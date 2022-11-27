6797bbe9d85479234003d2e7801916b15b8efe63
Bryan Miller <bmilcs@yahoo.com>
2022-11-27 15:27:04 -0500

    Add & style page layout, main menu card & footer

--- src/modules/app.js
+++ src/modules/app.js
@@ -2,6 +2,7 @@ import * as DOM from './dom';
 
 const start = () => {
   DOM.renderLayout();
+  DOM.renderMainMenu();
 };
 
 export { start };
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,14 +2,21 @@ import containerize from './utility/containerize';
 import makeElement from './utility/make-element';
 import '../scss/index.scss';
 import '../scss/footer.scss';
+import '../scss/menu.scss';
 import githubSVG from '../assets/github.svg';
-import battleshipImg from '../assets/battleship.svg';
+import battleshipLogoSVG from '../assets/battleship.svg';
+import battleshipIconSVG from '../assets/battleship-icon.svg';
+import battleshipAction from '../assets/battleship-action.jpg';
+
+//
+// main layout
+//
 
 const renderLayout = () => {
   containerize(
     document.querySelector('body'),
     prepHeader(),
-    prepMain(),
+    main,
     prepFooter()
   );
 };
@@ -17,9 +24,15 @@ const renderLayout = () => {
 const prepHeader = () => {
   return containerize(
     makeElement('header'),
-    makeElement('img', 'page-logo', 'battleship image', '', battleshipImg),
-    makeElement('h1', 'page-title', 'Battleship'),
-    makeElement('p', 'page-subtitle', 'The Classic Naval Combat Game')
+    makeElement('h3', 'header-title', 'Battleship'),
+    makeElement(
+      'div',
+      'header-logo',
+      'battleship image',
+      '',
+      '',
+      battleshipIconSVG
+    )
   );
 };
 
@@ -27,15 +40,59 @@ const prepMain = () => {
   return makeElement('main');
 };
 
+const main = prepMain();
+
 const prepFooter = () => {
   return containerize(
     makeElement('footer'),
     containerize(
       makeElement('a', '', '', '', 'https://github.com/bmilcs/odin-battleship'),
-      makeElement('img', 'github-svg', 'GitHub Logo', '', githubSVG),
+      makeElement('div', 'github-svg', 'GitHub Logo', '', '', githubSVG),
       makeElement('p', 'footer-p', 'bmilcs')
     )
   );
 };
 
-export { renderLayout };
+//
+// game play
+//
+
+const clearMain = () => {
+  while (main.firstChild) {
+    main.removeChild(main.lastChild);
+  }
+};
+
+const renderMainMenu = () => {
+  containerize(
+    main,
+    containerize(
+      'main-menu-card',
+      prepMainLogo(),
+      makeElement(
+        'p',
+        'instructions',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum impedit necessitatibus distinctio corrupti porro fugiat, odit vitae soluta itaque consequuntur at sed eveniet pariatur explicabo consectetur incidunt! Maxime ullam ipsa, laudantium possimus perspiciatis omnis pariatur velit odio eveniet sint repellendus minus, tenetur expedita dolores delectus eos doloribus saepe illo impedit?'
+      ),
+      makeElement('button', 'start-btn', 'Start Game')
+    )
+  );
+};
+
+const prepMainLogo = () => {
+  return containerize(
+    'main-logo-container',
+    makeElement('img', 'main-img', 'battleship image', '', battleshipAction),
+    containerize(
+      'logo-text-container',
+      makeElement('h1', 'main-title', 'Battleship'),
+      makeElement('p', 'main-subtitle', 'The Classic Naval Combat Game')
+    )
+  );
+};
+
+const renderBoard = (boardArr) => {
+  //
+};
+
+export { renderLayout, renderMainMenu };
