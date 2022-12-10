6c9ea50152acc288065fd8ac646cefc6b2d23229
Bryan Miller <bmilcs@yahoo.com>
2022-12-10 13:28:14 -0500

    Add comment & shorten hit() method

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,10 +1,12 @@
+//
+// Ship Factory
+//
+
 export default (shipLength) => {
   const length = shipLength;
   let hits = 0;
 
-  const hit = () => {
-    ++hits;
-  };
+  const hit = () => ++hits;
 
   const isSunk = () => {
     return length === hits ? true : false;
